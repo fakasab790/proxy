aa7330d9955373e652804ef39ab8628e911549d5
fakasab790 <78923561+fakasab790@users.noreply.github.com>
2021-02-11 19:28:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ app.use('/json/', createProxyMiddleware({
 }));
 
 // Start Proxy
-app.listen(() => {
-    console.log(`Starting Proxy `);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
+
+
