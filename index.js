d62fde7e4f4203794915e7d2e289050ae540e257
fakasab790 <78923561+fakasab790@users.noreply.github.com>
2021-02-11 18:27:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Configuration
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 const HOST = "localhost";
 const API_SERVICE_URL = "https://api.radialle.com/api/";
 
